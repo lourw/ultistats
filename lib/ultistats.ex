5b39fab9715161fde9f76c65ad0d98a8d15f9349
lourw <56288712+lourw@users.noreply.github.com>
2026-05-02 17:20:07 -0700

    initial commit

---
+++ lib/ultistats.ex
@@ -0,0 +1,18 @@
+defmodule Ultistats do
+  @moduledoc """
+  Documentation for `Ultistats`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Ultistats.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ultistats_test.exs
@@ -0,0 +1,8 @@
+defmodule UltistatsTest do
+  use ExUnit.Case
+  doctest Ultistats
+
+  test "greets the world" do
+    assert Ultistats.hello() == :world
+  end
+end
